3c5706417afcfc53daec34f7d9df6958ccbec292
kevin <kevin@paay.co>
2024-05-11 18:27:34 -0400

    add material ui

--- src/components/ui/navigation/SideNavBar.jsx
+++ src/components/ui/navigation/SideNavBar.jsx
@@ -47,26 +47,26 @@ export default function VerticalTabs() {
 
   return (
     <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 500 }}
     >
       <Tabs
         orientation="vertical"
-        variant="scrollable"
+        variant="fullHeight"
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
         sx={{ borderRight: 1, borderColor: 'divider' }}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="React" {...a11yProps(0)} />
+        <Tab label="Git" {...a11yProps(1)} />
+        <Tab label="Vue.js" {...a11yProps(2)} />
+        <Tab label="Golang" {...a11yProps(3)} />
         <Tab label="Item Five" {...a11yProps(4)} />
         <Tab label="Item Six" {...a11yProps(5)} />
         <Tab label="Item Seven" {...a11yProps(6)} />
       </Tabs>
       <TabPanel value={value} index={0}>
-        Item One
+        Blogs on react
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
