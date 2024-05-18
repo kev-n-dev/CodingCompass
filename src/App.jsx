c03bece3641af0dfa9123870f6919c5dec7bbfa7
kevin <kevin@paay.co>
2024-05-17 20:30:42 -0400

    added neomorph nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/ui/navigation/TopNav';
-import Blog from './components/ui/blogs/Blog';
-import { Link, Routes, Route } from 'react-router-dom';
+import '../src/components/assets/css/base.scss'
 import HomePage from './components/pages/HomePage';
 import BlogsPage from './components/pages/BlogsPage';
 import AboutPage from './components/pages/AboutPage';
-import post1 from './components/ui/blogs/blog-post.1.md';
-import post2 from './components/ui/blogs/blog-post.2.md';
-import post3 from './components/ui/blogs/blog-post.3.md';
+import Navigation from './components/ui/navigation/Navigation';
+import NotFoundPage from './components/pages/notFound';
+
 function App() {
- 
-  
-  
-  const posts = [
-    {
-      file: './components/ui/blogs/blog-post.1.md',
-      title: 'Featured post',
-      date: 'Nov 12',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-      image: 'https://source.unsplash.com/random?wallpapers',
-      imageText: "imageText",
-      imageLabel: 'Image Text',
-      linkText: 'Continue reading…',
-      mainFeature: true,
-      feature: false,
-    }, {
-      file: './components/ui/blogs/blog-post.2.md',
-      title: 'Post title',
-      date: 'Nov 11',
-      imageText: "imageText",
-
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-      image: 'https://source.unsplash.com/random?wallpapers',
-      imageLabel: 'Image Text',
-      linkText: 'Continue reading…',
-      mainFeature: false,
-      feature: true,
-    },
-    {
-      file: './components/ui/blogs/blog-post.2.md',
-      title: 'Post title',
-      date: 'Nov 11',
-      imageText: "imageText",
-
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-      image: 'https://source.unsplash.com/random?wallpapers',
-      imageLabel: 'Image Text',
-      linkText: 'Continue reading…',
-      mainFeature: false,
-      feature: true,
-    },
-    {
-      file: './components/ui/blogs/blog-post.3.md',
-      title: 'Another post',
-      date: 'Nov 10',
-      imageText: "imageText",
-
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-      image: 'https://source.unsplash.com/random?wallpapers',
-      imageLabel: 'Image Text',
-      linkText: 'Continue reading…',
-      mainFeature: false,
-      feature: false,
-    },
-   ];
-  
-   return (
-    <>
-      <Router>
-
-        <Routes>
-          <Route path="/CodingCompass" element={<HomePage />} />
-          <Route path="/blogs" element={<BlogsPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-
-      </Router>
-      <Blog 
-          posts={posts} />
+  return (
 
 
-    </>
-
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/CodingCompass" element={ <HomePage />}/>
+        <Route path="/CodingCompass/blogs" element={<BlogsPage />}/>
+        <Route path="/CodingCompass/about"element={<AboutPage />}/>
+        <Route path="*" element={<NotFoundPage />} /> 
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/pages/BlogsPage.jsx
+++ src/components/pages/BlogsPage.jsx
@@ -1,10 +1,78 @@
 // BlogsPage.js
 import React from 'react';
- import VerticalTabs from '../ui/navigation/SideNavBar';
+import Blog from '../ui/blogs/Blog';
+
+import post1 from '../assets/logs/vite.md';
+import post2 from '../ui/blogs/blog-post.2.md';
+import post3 from '../ui/blogs/blog-post.3.md';
+
+
+const posts = [
+    {
+      file: post1,
+      title: 'Featured post',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random?wallpapers',
+      imageText: "imageText",
+      imageLabel: 'Image Text',
+      linkText: 'Continue reading…',
+      mainFeature: true,
+      feature: false,
+    }, {
+      file: post2,
+      title: 'Post title',
+      date: 'Nov 11',
+      imageText: "imageText",
+
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random?wallpapers',
+      imageLabel: 'Image Text',
+      linkText: 'Continue reading…',
+      mainFeature: false,
+      feature: true,
+    },
+    {
+      file: post3,
+      title: 'Post title2',
+      date: 'Nov 11',
+      imageText: "imageText",
+
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random?wallpapers',
+      imageLabel: 'Image Text',
+      linkText: 'Continue reading…',
+      mainFeature: false,
+      feature: true,
+    },
+    {
+      file: './components/ui/blogs/blog-post.3.md',
+      title: 'Another post',
+      date: 'Nov 10',
+      imageText: "imageText",
+
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random?wallpapers',
+      imageLabel: 'Image Text',
+      linkText: 'Continue reading…',
+      mainFeature: false,
+      feature: false,
+    },
+   ];
+
 
 const BlogsPage = () => {
     return (
-        <VerticalTabs></VerticalTabs>
+        <>
+
+
+      <Blog
+      posts={posts} />
+      </>
      );
 }
 
