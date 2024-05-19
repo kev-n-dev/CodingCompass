4e7cacac0d85c62482329c183243363a1f41835e
kevin <kevin@paay.co>
2024-05-19 16:11:19 -0400

    changed to hash router for github

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.md"],
+  homepage: "https://coding-corps.github.io/",
 
   base: '/CodingCompass/',
 })
