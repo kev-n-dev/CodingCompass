be6a7af5409f378add94795f5f706ce35ee65846
kevin <kevin@paay.co>
2024-05-27 09:43:18 -0400

    updated build chunk limit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  chunkSizeWarningLimit: 3000, // Set the limit in KB
-
   assetsInclude: ["**/*.md"],
   homepage: "https://coding-corps.github.io/CodingCompass/",
-
   base: '/CodingCompass/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Example: splitting vendor libraries
+            if (id.includes('react')) {
+              return 'vendor-react';
+            }
+            if (id.includes('lodash')) {
+              return 'vendor-lodash';
+            }
+            // You can add more custom logic to split other large libraries
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Set the limit in KB as needed
+  }
 })
